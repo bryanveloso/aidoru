a4f0dc0dca3b65d04f95ec2eef040914a85b3ec7
Bryan Veloso <bryan@revyver.com>
2012-01-30 00:42:00 -0800

    Fleshing out the view methods.

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import random
+
 from flask import Flask, jsonify
+from images import images
 
 
 app = Flask(__name__)
 app.debug = True
+
+# The number of idols we drop by default.
+DEFAULT_BOMB = 5
 
 
 @app.route('/')
@@ -10,6 +16,7 @@ def index():
     d = {
         'resources': {
             '/random': 'A random picture of a Japanese idol.',
+            '/bomb': 'Bomb idols.',
             '/count': 'The number of idols we have.'
         },
         'source': 'https://github.com/bryanveloso/aidoru'
@@ -19,12 +26,20 @@ def index():
 
 @app.route('/random')
 def random_idol():
-    pass
+    idol = random.sample(images, 1)
+    return jsonify({'idol': idol})
+
+
+@app.route('/bomb')
+def bomb_idols():
+    idols = random.sample(images, DEFAULT_BOMB)
+    return jsonify({'idols': idols})
 
 
 @app.route('/count')
 def count_idols():
-    pass
+    count = len(images)
+    return jsonify({'count': count})
 
 
 if __name__ == '__main__':
