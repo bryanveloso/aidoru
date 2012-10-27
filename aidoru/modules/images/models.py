cbd28be7f5df2a43d7eca1715f6e51daf21c1158
Bryan Veloso <bryan@revyver.com>
2012-10-27 15:23:37 -0700

    Random image manager.

--- aidoru/modules/images/models.py
+++ aidoru/modules/images/models.py
@@ -2,16 +2,19 @@
 import logging
 logger = logging.getLogger(__name__)
 
+import random
+
 from django.db import models
+from django.db.models import Count
 
 from model_utils.models import TimeStampedModel
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __unicode__(self):
-        return u'%s' % self.name
+class ImageManager(models.Manager):
+    def random(self):
+        count = self.aggregate(count=Count('id'))['count']
+        random_index = random.randint(0, count - 1)
+        return self.all()[random_index]
 
 
 class Image(TimeStampedModel):
@@ -20,8 +23,18 @@ class Image(TimeStampedModel):
     url = models.URLField(verify_exists=True)
 
     # Categorization.
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField('Tag')
     idols = models.TextField(blank=True)
+
+    # Managers
+    objects = ImageManager()
 
     def __unicode__(self):
         return u'%s' % self.url
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return u'%s' % self.name
