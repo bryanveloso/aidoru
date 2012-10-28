2db88b8a68c829286efc39be2aba6b787c1fda7a
Bryan Veloso <bryan@revyver.com>
2012-10-27 18:33:36 -0700

    Bomb idols.

--- aidoru/modules/images/views.py
+++ aidoru/modules/images/views.py
@@ -27,14 +27,24 @@ class RandomImageChoiceView(JSONResponseMixin, View):
     def get(self, request, *args, **kwargs):
         self.object = Image.objects.random()
 
-        json_dict = {
-            'token': self.object.token,
-            'url': self.object.url,
-        }
+        json_dict = {}
+        json_dict['idol']['token'] = self.object.token
+        json_dict['idol']['url'] = self.object.url
         return self.render_json_response(json_dict)
 
 
 class CountTheImagesView(JSONResponseMixin, View):
     def get(self, request, *args, **kwargs):
-        json_dict = {'count': Image.objects.count()}
+        json_dict = {'count': Image.objects.all().count()}
+        return self.render_json_response(json_dict)
+
+
+class BombIdolsView(JSONResponseMixin, View):
+    def get(self, request, *args, **kwargs):
+        bombs = int(self.request.GET.get('id', 5))
+        count = Image.objects.all().count()
+        random_ids = sample(xrange(1, count), bombs)
+
+        json_dict = {}
+        json_dict['idols'] = [image.url for image in Image.objects.filter(id__in=random_ids)]
         return self.render_json_response(json_dict)
--- aidoru/urls.py
+++ aidoru/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from modules.images.views import (CountTheImagesView, IndexView,
-    RandomImageChoiceView)
+from modules.images.views import (BombIdolsView, CountTheImagesView,
+    IndexView, RandomImageChoiceView)
 
 
 admin.autodiscover()
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     # Endpoints (backwards-compatible with the Flask version).
     url(r'^count/$', CountTheImagesView.as_view(), name='count'),
     url(r'^random/$', RandomImageChoiceView.as_view(), name='random'),
+    url(r'^bomb/$', BombIdolsView.as_view(), name='bomb'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
