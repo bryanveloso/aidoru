a426cdf7d33e00a02748d556bd8124c11df1c894
Bryan Veloso <bryan@revyver.com>
2012-10-27 21:30:01 -0700

    Convert create_token() to a signal.

--- aidoru/modules/images/models.py
+++ aidoru/modules/images/models.py
@@ -5,7 +5,8 @@ logger = logging.getLogger(__name__)
 import random
 
 from django.db import models
-from django.db.models import Count
+from django.db.models import Count, signals
+from django.dispatch import receiver
 
 from model_utils.models import TimeStampedModel
 from modules.people.models import Idol
@@ -34,16 +35,14 @@ class Image(TimeStampedModel):
     def __unicode__(self):
         return u'%s' % self.token
 
-    def save(self, *args, **kwargs):
-        super(Image, self).save(*args, **kwargs)
-        self.token = self.create_token()
-        self.save()
 
-    def create_token(self):
-        from multiprocessing.dummy import list
-        ALPHABET = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+@receiver(signals.post_save, sender=Image)
+def create_token(instance, created, *args, **kwargs):
+    from multiprocessing.dummy import list
+    ALPHABET = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
 
-        i = self.id
+    if created:
+        i = instance.id
         if i == 0:
             return ALPHABET[0]
         s = ''
@@ -51,4 +50,5 @@ class Image(TimeStampedModel):
         while i > 0:
             s += ALPHABET[i % base]
             i /= base
-        return s[::-1]
+        instance.token = s[::-1]
+        instance.save()
