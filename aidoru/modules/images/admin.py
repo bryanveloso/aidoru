55807db3c5b7b083a024c9580978fb16c71af8dc
Bryan Veloso <bryan@revyver.com>
2012-10-27 20:58:34 -0700

    Removing the rest of the vestigal tag things.

--- aidoru/modules/images/admin.py
+++ aidoru/modules/images/admin.py
@@ -7,8 +7,8 @@ from django.contrib import admin
 
 import selectable.forms as selectable
 
-from modules.images.models import Image, Tag
-from modules.images.lookups import IdolLookup, TagLookup
+from modules.images.models import Image
+from modules.images.lookups import IdolLookup
 
 
 class ImageAdminForm(forms.ModelForm):
@@ -16,7 +16,6 @@ class ImageAdminForm(forms.ModelForm):
         model = Image
         widgets = {
             'idols': selectable.AutoCompleteSelectMultipleWidget(lookup_class=IdolLookup),
-            'tags': selectable.AutoCompleteSelectMultipleWidget(lookup_class=TagLookup)
         }
 
 
@@ -25,4 +24,3 @@ class ImageAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Image, ImageAdmin)
-admin.site.register(Tag)
--- aidoru/modules/images/lookups.py
+++ aidoru/modules/images/lookups.py
@@ -1,7 +1,6 @@
 from selectable.base import ModelLookup
 from selectable.registry import registry
 
-from modules.images.models import Tag
 from modules.people.models import Idol
 
 
@@ -9,9 +8,3 @@ class IdolLookup(ModelLookup):
     model = Idol
     search_fields = ('name__icontains',)
 registry.register(IdolLookup)
-
-
-class TagLookup(ModelLookup):
-    model = Tag
-    search_fields = ('name__icontains',)
-registry.register(TagLookup)
