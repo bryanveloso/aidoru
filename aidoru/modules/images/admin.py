ac3559a3f16bef8573f8c834ca6733d6ba8360d3
Bryan Veloso <bryan@revyver.com>
2012-10-27 21:29:45 -0700

    Expand on the admin a bit.

--- aidoru/modules/images/admin.py
+++ aidoru/modules/images/admin.py
@@ -21,6 +21,19 @@ class ImageAdminForm(forms.ModelForm):
 
 class ImageAdmin(admin.ModelAdmin):
     form = ImageAdminForm
+    list_display = ['token', 'linked_url', 'attributed_idols', 'attributed_tags']
+    list_filter = ['idols']
 
+    def attributed_idols(self, obj):
+        return ', '.join([i.name for i in obj.idols.all()])
+    attributed_idols.short_description = 'idols'
 
+    def attributed_tags(self, obj):
+        return ', '.join([t.name for t in obj.tags.all()])
+    attributed_tags.short_description = 'tags'
+
+    def linked_url(self, obj):
+        return '<a href="%s" target="_blank">%s</a>' % (obj.url, obj.url)
+    linked_url.allow_tags = True
+    linked_url.short_description = 'URL'
 admin.site.register(Image, ImageAdmin)
